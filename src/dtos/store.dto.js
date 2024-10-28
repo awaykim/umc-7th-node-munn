dae7f327fb6f6e133f2ddea0afa1facf26e30fc9
awaykim <wonj010101@gmail.com>
2024-10-28 18:08:21 +0900

    feat: 가게 추가

--- src/dtos/store.dto.js
+++ src/dtos/store.dto.js
@@ -1,3 +1,24 @@
-export const bodyNewStore = () => {
-    
+export const bodyNewStore = (body) => {
+    return {
+        name: body.name,
+        regionId: body.regionId,
+        address: body.address || "",
+        telNum: body.telNum,
+        foodCategoryId: body.foodCategoryId,
+    };
+}
+
+export const responseAddStore = ({store, region, foodCategory}) => {
+    const storeData = store[0];
+    const regionData = region[0];
+    const foodCategoryData = foodCategory[0];
+    return {
+        id: storeData.id,
+        name: storeData.name,
+        address: storeData.address,
+        telNum: storeData.tel_num,
+        status: storeData.status,
+        region: regionData,
+        foodCategory: foodCategoryData,
+    }
 }
--- src/repositories/store.repository.js
+++ src/repositories/store.repository.js
@@ -1,5 +1,97 @@
+import { pool } from "../db.config.js";
+
+// Repository를 통해 DB에 접근
+const now = new Date();
+
+// Store 데이터 삽입
+export const addStore = async (data) => {
+  const conn = await pool.getConnection();
+  try {
 
 
-export const addStroe = () => {
+    const [result] = await pool.query(
+      `INSERT INTO store (name, address, tel_num, created_at, category_id, region_id, status) VALUES (?, ?, ?, ?, ?, ?, ?);`,
+      [
+        data.name,
+        data.address,
+        data.telNum,
+        now,
+        data.foodCategoryId,
+        data.regionId,
+        "active"
+      ]
+    );
+    return result.insertId;
+  } catch (err) {
+    throw new Error(
+      `오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err})`
+    );
+  } finally {
+    conn.release();
+  }
+};
 
-}
+
+export const getStore = async (StoreId) => {
+  const conn = await pool.getConnection();
+
+  try {
+    const [store] = await conn.query(
+      `SELECT * FROM store WHERE id = ?;`,
+      [StoreId]
+    );
+
+    if (store.length == 0) {
+      return null;
+    }
+    console.log("getStore: ", store)
+    return store;
+  } catch (err) {
+    throw new Error(
+      `오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err})`
+    );
+  } finally {
+    conn.release();
+  }
+};
+
+
+export const getRegion = async (StoreId) => {
+  const conn = await pool.getConnection();
+
+  try {
+    const [region] = await conn.query(
+      `SELECT store.region_id, region.name ` +
+      `FROM store JOIN region ON store.region_id = region.id ` +
+      `WHERE store.id = ?`,
+      [StoreId]
+    );
+    return region;
+  } catch (err) {
+    throw new Error(
+      `오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err})`
+    );
+  } finally {
+    conn.release();
+  }
+};
+
+export const getFoodCategory = async (StoreId) => {
+    const conn = await pool.getConnection();
+
+    try {
+        const [foodCategory] = await conn.query(
+            `SELECT store.category_id, fc.name ` +
+                `FROM store JOIN food_category fc on store.category_id = fc.id ` +
+                `WHERE store.id = ?`,
+            [StoreId]
+        );
+        return foodCategory;
+    } catch (err) {
+        throw new Error(
+            `오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err})`
+        );
+    } finally {
+        conn.release();
+    }
+};
