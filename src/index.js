c8732aac7555df5315b584586997b6ab617ca9d1
awaykim <wonj010101@gmail.com>
2024-11-14 16:16:57 +0900

    feat: 진행 중 미션 완료 처리

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-import express from 'express'          // -> ES Module
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { handleUserSignUp } from "./controllers/user.controller.js";
+import { handleStoreAdd } from "./controllers/store.controller.js";
+import {
+    handleReviewWrite,
+    handleListStoreReviews,
+    handleUserReivew,
+} from "./controllers/review.controller.js";
+import {
+    handleMissionAdd,
+    handleUserMissionOngiong,
+    handleListStoreMissions,
+    hanldeListUserOngoingMissions,
+    handleCompleteOngoingMission,
+} from "./controllers/mission.controller.js";
 
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.use(cors()); // cors 방식 허용
+app.use(express.static("public")); // 정적 파일 접근
+app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
+app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
+
+app.get("/", (req, res) => {
+    res.send("서버 시작됨");
+});
+
+app.post("/api/v1/users/signup", handleUserSignUp);
+app.post("/api/v1/store/add", handleStoreAdd);
+app.post("/api/v1/store/:store_id/review", handleReviewWrite);
+app.post("/api/v1/store/mission/add", handleMissionAdd);
+app.post("/api/v1/user/:user_id/mission/ongoing", handleUserMissionOngiong);
+app.get("/api/v1/stores/:storeId/reviews", handleListStoreReviews);
+app.get("/api/v1/user/:userId/reviews", handleUserReivew);
+app.get("/api/v1/user/:storeId/missions", handleListStoreMissions);
+app.get("/api/v1/user/:userId/missions/ongoing", hanldeListUserOngoingMissions);
+app.patch("/api/v1/user/:userId/missions/complete", handleCompleteOngoingMission);
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
---
+++ src/services/mission.service.js
@@ -0,0 +1,68 @@
+import {
+    responseMissionToStore,
+    responseUserMissionOngiong,
+    responseFromMissions,
+} from "../dtos/mission.dto.js";
+import {
+    addMissionToStore,
+    getMissionFromStore,
+    addUserMissionOngoing,
+    getUserMission,
+    getAllStoreMissions,
+    getAllUserOngoinhMissions,
+    updateOngoingToComplete,
+} from "../repositories/mission.repository.js";
+
+export const newMission = async (data) => {
+    const joinMissionId = await addMissionToStore({
+        storeId: data.storeId,
+        reward: data.reward,
+        deadline: data.deadline,
+        missionSpec: data.missionSpec,
+    });
+
+    const mission = await getMissionFromStore(joinMissionId);
+
+    return responseMissionToStore({ mission });
+};
+
+export const newUserMission = async (data) => {
+    const joinUserMissionId = await addUserMissionOngoing({
+        memberId: data.memberId,
+        missionId: data.missionId,
+    });
+    if (joinUserMissionId === null) {
+        throw new Error("이미 진행 중인 미션입니다.");
+    }
+    const userMission = await getUserMission(joinUserMissionId);
+
+    const memberId = data.memberId;
+
+    return responseUserMissionOngiong(userMission, memberId);
+};
+
+export const listStoreMissions = async (storeId, cursor) => {
+    const missions = await getAllStoreMissions(storeId, cursor);
+    return responseFromMissions(missions);
+};
+
+export const listUserOngoingMissions = async (memberId, cursor) => {
+    const ongoingMissions = await getAllUserOngoinhMissions(memberId, cursor);
+    return responseFromMissions(ongoingMissions);
+}
+
+export const CompleteOngoingMission = async (data) => {
+    const userMissionId = await updateOngoingToComplete({
+        memberId: data.memberId,
+        missionId: data.missionId,
+    });
+    if (userMissionId == null) {
+        throw new Error("진행 중인 미션이 아닙니다.");
+    }
+
+    const userMission = await getUserMission(userMissionId);
+
+    const memberId = data.memberId;
+
+    return responseUserMissionOngiong(userMission, memberId);
+}
